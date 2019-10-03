23e2ea1f368764f6e16fffdb66121d862bc30190
navinkumar.c <navinkumar.c@zohocorp.com>
2019-10-03 11:47:44 +0530

    signalling server

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const uuid = require("./uuid");
+const httpLogger = require("./http-logger");
+const bodyParser = require("body-parser");
+const peerParty = require("./services/peer-party");
 
+// modules
+
+// instantiate
+const app = express();
+// adding unique id for every request
+app.use(uuid);
+
+// add body parsers
+app.use(bodyParser.json());
+
+// log all http req/res 
+app.use(httpLogger.req);
+app.use(httpLogger.res);
+
+app.use("/party",peerParty);
+
+
+// listen for request
+app.listen(process.env.port || 8000, function () {
+    console.log('server running on port', process.env.port || 8000);
+});
