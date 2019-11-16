7e1e09d525de2e6fa6929678141c72a6e570d66f
1995navinkumar <1995navinkumar@gmail.com>
2019-11-16 16:49:58 +0530

    partyJoiner

--- app/peer-party/join-party.js
+++ app/peer-party/join-party.js
@@ -1,64 +1,11 @@
-(function (){
+async function partyJoiner(partyName, actions) {
     var slavePeer;
-    
-    async function joinParty(partyName) {
-        await Socket({ username: "harish" });
-        sessionStorage.setItem("partyId", partyName);
-    }
-    
-    chrome.runtime.onMessage.addListener((message) => {
-        if(message.type == "join-party") {
-            joinParty(message.partyName);
-        }
-    });
-    
-    function streamReceiver({ streams: [stream] }) {
-        log(stream);
-        if (audioPlayer.srcObject) return;
-        audioPlayer.srcObject = stream;
-        audioPlayer.play();
-    }
-    
-    async function sendAudio() {
-        log("add slave track");
-        const gumStream = await navigator.mediaDevices.getUserMedia({
-            audio: true
-        });
-    
-        for (const track of gumStream.getTracks()) {
-            log(track);
-            slavePeer.addTrack(track, gumStream);
-        }
-    }
-    
-    
-    function createPeerConnection(iceServers) {
-        slavePeer = new RTCPeerConnection({
-            iceServers
-        });
-        slavePeer.onnegotiationneeded = async function () {
-            console.log("negotiation needed event slave");
-            log(slavePeer.signalingState);
-        }
-        slavePeer.ontrack = streamReceiver;
-        slavePeer.onicecandidate = handleSlaveICECandidateEvent
-    }
-    
-    function handleSlaveICECandidateEvent(event) {
-        log("ice candidate handling");
-        if (event.candidate) {
-            signal({
-                action: "offer-candidate",
-                candidate: event.candidate
-            })
-        }
-    }
-    
-    let actions = {
-        "connection" : function(data){
+
+    Object.assign(actions, {
+        "connection": function (data) {
             console.log(data);
-            sessionStorage.setItem("uuid",data.uuid);
-                actions["connection-success"](data);           
+            sessionStorage.setItem("uuid", data.uuid);
+            actions["connection-success"](data);
         },
         "connection-success": function () {
             signal({
@@ -68,14 +15,14 @@
         },
         "answer-request": async function acceptOfferAndSendAnswer(data) {
             var desc = new RTCSessionDescription(data.offer);
-    
+
             createPeerConnection(iceServers);
-    
+
             // If the connection isn't stable yet, wait for it...
-    
+
             if (slavePeer.signalingState != "stable") {
                 log("  - But the signaling state isn't stable, so triggering rollback");
-    
+
                 // Set the local and remove descriptions for rollback; don't proceed
                 // until both return.
                 await Promise.all([
@@ -87,16 +34,16 @@
                 log("  - Setting remote description");
                 // await sendAudio();
                 await slavePeer.setRemoteDescription(desc);
-    
+
                 let answer = await slavePeer.createAnswer(constraints);
                 slavePeer.setLocalDescription(answer);
-    
+
                 signal({
                     action: "answer",
                     answer: slavePeer.localDescription
                 });
             }
-    
+
         },
         "set-remote-candidate": function setRemoteCandidate({ candidate: remoteCandidate }) {
             if (slavePeer.remoteDescription) {
@@ -105,5 +52,51 @@
                 slavePeer.addIceCandidate(candidate)
             }
         }
-    };
-})();
+    });
+
+    await Socket({ username: "harish" });
+    sessionStorage.setItem("partyId", partyName);
+
+    function streamReceiver({ streams: [stream] }) {
+        log(stream);
+        if (audioPlayer.srcObject) return;
+        audioPlayer.srcObject = stream;
+        audioPlayer.play();
+    }
+
+    // async function sendAudio() {
+    //     log("add slave track");
+    //     const gumStream = await navigator.mediaDevices.getUserMedia({
+    //         audio: true
+    //     });
+
+    //     for (const track of gumStream.getTracks()) {
+    //         log(track);
+    //         slavePeer.addTrack(track, gumStream);
+    //     }
+    // }
+
+
+    function createPeerConnection(iceServers) {
+        slavePeer = new RTCPeerConnection({
+            iceServers
+        });
+        slavePeer.onnegotiationneeded = async function () {
+            console.log("negotiation needed event slave");
+            log(slavePeer.signalingState);
+        }
+        slavePeer.ontrack = streamReceiver;
+        slavePeer.onicecandidate = handleSlaveICECandidateEvent
+    }
+
+    function handleSlaveICECandidateEvent(event) {
+        log("ice candidate handling");
+        if (event.candidate) {
+            signal({
+                action: "offer-candidate",
+                candidate: event.candidate
+            })
+        }
+    }
+
+};
