a443341bfd7081b463e51873f7f8d7c23d3988e4
1995navinkumar <1995navinkumar@gmail.com>
2020-01-26 10:50:21 +0530

    ping pong

--- app/socket.js
+++ app/socket.js
@@ -31,10 +31,26 @@ function Socket(server, wss) {
     });
 
     function attachEvents(connection, ws) {
+        ws.isAlive = true;
         ws.on("message", utils.pipe(utils.parser, connection.loadBalancer));
         ws.on("close", ConnectionManager.terminateConnection(connection));
         ws.on("error", logger.error);
+        ws.on('pong', heartbeat);
     }
+
+    function noop() { }
+
+    function heartbeat() {
+        this.isAlive = true;
+    }
+
+    const interval = setInterval(function ping() {
+        wss.clients.forEach(function each(ws) {
+            if (ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping(noop);
+        });
+    }, 30000);
 }
 
 module.exports = Socket;
