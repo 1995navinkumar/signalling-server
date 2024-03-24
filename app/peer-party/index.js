8a351e93019e11d3e718ee723c170b4f8585eb94
1995navinkumar <navin.kumar@razorpay.com>
2024-03-24 13:20:40 +0530

    add pathname

--- app/peer-party/index.js
+++ app/peer-party/index.js
@@ -1,69 +1,70 @@
 sessionStorage.clear();
 
 const servers = {
-    urls: "stun:stun1.l.google.com:19302"
-}
+  urls: "stun:stun1.l.google.com:19302",
+};
 
 const turnServer = {
-    urls: 'turn:144.24.128.160:7001',
-    credential: 'rtc',
-    username: 'navin'
-}
+  urls: "turn:144.24.128.160:7001",
+  credential: "rtc",
+  username: "navin",
+};
 
 const iceServers = [servers, turnServer];
 
 const log = console.log;
 
 const constraints = {
-    audio: true
+  audio: true,
 };
 
 const offerOptions = {
-    offerToReceiveAudio: 1,
+  offerToReceiveAudio: 1,
 };
 
 let socket;
 
-function Socket({username}) {
-    return new Promise((resolve, reject) => {
-        var hostName = location.hostname;
-        var connection = new WebSocket(`ws://${hostName}:8080`,username);
-        connection.onopen = function (e, f) {
-            log("socket connection established ");
-            resolve(connection);
-        }
-        connection.onmessage = pipe(messageParser, actionInvoker);
-        connection.onerror = function (e) {
-            log("error in connection establishment");
-            reject(e);
-        }
-        socket = connection;
-    });
-};
-
-var actions = {
-    "connection" : function(data){
-        sessionStorage.setItem("uuid",data.uuid);
-        actions["connection-success"](data);
-    }
+function Socket({ username }) {
+  return new Promise((resolve, reject) => {
+    var hostName = location.hostname;
+    var pathname = location.pathname;
+    var connection = new WebSocket(`ws://${hostName}/${pathname}/ws`, username);
+    connection.onopen = function (e, f) {
+      log("socket connection established ");
+      resolve(connection);
+    };
+    connection.onmessage = pipe(messageParser, actionInvoker);
+    connection.onerror = function (e) {
+      log("error in connection establishment");
+      reject(e);
+    };
+    socket = connection;
+  });
 }
 
+var actions = {
+  connection: function (data) {
+    sessionStorage.setItem("uuid", data.uuid);
+    actions["connection-success"](data);
+  },
+};
+
 function signal(message) {
-    socket.send(processMessage(message));
+  socket.send(processMessage(message));
 }
 
 function messageParser(message) {
-    return JSON.parse(message.data);
+  return JSON.parse(message.data);
 }
 
 function actionInvoker(data) {
-    var action = data.action;
-    action ? actions[action](data) : log(data);
+  var action = data.action;
+  action ? actions[action](data) : log(data);
 }
 
 function processMessage(message) {
-    var clientId = sessionStorage.getItem("uuid");
-    var partyId = sessionStorage.getItem("partyId");
-    var finalMessage = Object.assign({}, { clientId, partyId }, message);
-    return JSON.stringify(finalMessage);
+  var clientId = sessionStorage.getItem("uuid");
+  var partyId = sessionStorage.getItem("partyId");
+  var finalMessage = Object.assign({}, { clientId, partyId }, message);
+  return JSON.stringify(finalMessage);
 }
